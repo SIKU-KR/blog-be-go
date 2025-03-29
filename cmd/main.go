93198799d554f67456f6739f00cc976e2069a4d8
SIKU-KR <peter012677@naver.com>
2025-03-29 13:53:06 +0900

    feat: 로그인 핸들러 테스트 추가 (dev)
    - 로그인 성공, 실패, 잘못된 요청에 대한 테스트 케이스 추가
    - 테스트를 위한 helper 함수 (givenTestContext, givenTestEnv,
      thenAssertResponseJSON) 추가
    - 세션 관리를 위한 memstore 의존성 추가
    - gob.Register(time.Time{}) 추가하여 세션 저장 문제 해결

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"encoding/gob"
+	"os"
+	"time"
+
 	"bumsiku/controller"
 	"bumsiku/internal/config"
-	"os"
 )
 
 func main() {
 	if os.Getenv("GO_ENV") != "production" {
 		config.LoadEnv()
 	}
+
+	gob.Register(time.Time{})
 
 	r := controller.SetupRouter()
 	r.Run()
